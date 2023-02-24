39e0ad235a8e5527ca7775108d1b461ac8666434
fullstackstef <fullstackstef@gmail.com>
2023-02-24 18:00:03 -0500

    Added google analytics

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "995f32063f2ab428"; }
+/******/ 		__webpack_require__.h = function() { return "629966799290a6ed"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 import type { AppProps } from 'next/app'
 
 import Layout from '@/components/layout/Layout'
@@ -6,6 +7,7 @@ import Layout from '@/components/layout/Layout'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </Layout>
   )
